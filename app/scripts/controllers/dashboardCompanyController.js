7e3d58aa571f57963adc56eaad494e1f491a9903
Goh Bing Han <stiucsib86@hotmail.com>
2013-07-27 16:48:49 +0800

    Fixes to previous commit.

--- app/scripts/controllers/dashboardCompanyController.js
+++ app/scripts/controllers/dashboardCompanyController.js
@@ -1,26 +1,24 @@
 'use strict';
 
-function DashboardCompanyNotificationCtrl($http, $location, $rootScope, $scope) {
+function DashboardCompanyNotificationCtrl($http, $location, $rootScope, $scope, $routeParams) {
 
-  $scope.datepicker = {date: new Date("2012-09-01T00:00:00.000Z")};
-
-  $scope.GetProfileSettings = function() {
+  $scope.GetNotification = function() {
     $http({
       method: 'GET',
       withCredentials: true,
       url: $rootScope._app.url.api + 'user/',
       params: {
         //callback: 'JSON_CALLBACK'
+        notification_id: $routeParams.notification_id
       }
     }).success(function(data, status, headers, config) {
-      $rootScope.auth = data;
     }).error(function(data, status, headers, config) {
       console.warn(data);
     });
   };
 
   (function() {
-    $scope.GetProfileSettings();
+    $scope.GetNotification();
   })();
 
 }
